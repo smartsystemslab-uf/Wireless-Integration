3760dc2035ea8c36114c95e79b42db55d52464a2
Eddie Devlin <edevlin64@gmail.com>
2021-05-30 13:25:39 -0400

    App command to ESP successful

--- server-esp8266-tcp.py
+++ server-esp8266-tcp.py
@@ -1,6 +1,6 @@
-# in order to reset this program, delete the terminal and restart the script
+# in order to reset this program, delete the terminal and restart the script 
 
-# this script is for testing server connection to the esp8266
+# this script is for testing server connection to the esp8266 and only works with one ESP chip - will need to be serve multiple ESP clients
 
 import socket
 #from mobile-cmd_conversion import convertToTwist
@@ -11,28 +11,41 @@ import socket
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)    # TODO SOCK_STREAM is for TCP, we need to change to SOCK_DGRAM for udp
 SERVER_HOSTNAME = socket.gethostname()
 SERVER_IP = socket.gethostbyname(SERVER_HOSTNAME) 
-print ("Socket successfully created. The server Hostname is", SERVER_HOSTNAME, "and the IP address is", SERVER_IP, "\n")
+print ("Socket successfully created. The server Hostname is", SERVER_HOSTNAME, "and the Server IP address is", SERVER_IP, "\n")
 port = 8001
 s.bind(('', port))
 print ("Socket binded to port %s\n" %(port))
-s.listen(1)
-print ("Socket is listening... waiting for the ESP to connect\n")
 
+# connect to ESP
+s.listen(1)
+print ("Socket is listening for the ESP to connect. Run the ESP program.\n")
+esp, ESP_IP = s.accept()
+print ('Got connection from the ESP at', ESP_IP )
+esp.send('Thank you for connecting\n'.encode('utf-8'))
+# receive confirmation message from ESP
+data = esp.recv(1024)
+data = data.decode('utf-8')
+print(data)
+
+# connect to the mobile app
+s.listen(1)
+print ("Socket is listening for the mobile app. Press connect on the mobile app\n")
+app, APP_IP = s.accept()
+print ('Got connection from the mobile app at', APP_IP )
+app.send('Thank you for connecting\n'.encode('utf-8'))
+print("Now send commands using the mobile app\n")
+
+# main loop
 while True:
-    esp, ESP_IP = s.accept()
-    print ('Got connection from', ESP_IP )
-    esp.send('Thank you for connecting\n'.encode('utf-8'))
     try:
-        data = esp.recv(1024)                                    # TODO this is going to be from the mobile app
-        data = data.decode('utf-8')
-        while True:
-            print(data)
-            if not data:
-                print("Disconnected from ", ESP_IP)
-                break
-            esp.send('This is test message A\n'.encode('utf-8'))      # TODO for testing purposes
-            esp.send('This is test message B\n'.encode('utf-8'))      # TODO for testing purposes
-            
+        # main message loop
+        app_cmd = app.recv(1024)
+        app_cmd = app_cmd.decode('utf-8')
+        if not data:
+            print("Disconnected from ", APP_IP)
+            break
+        esp.send(app_cmd.encode('utf-8'))
+        print("Command sent to the ESP:", app_cmd)
 
     except KeyboardInterrupt:
             print("keyboard interrupted")
@@ -41,4 +54,4 @@ while True:
     except Exception as error:
             print("Failed to insert record into Laptop table {}".format(error))
             exit()
-                                                                                  
+                                                                                    
